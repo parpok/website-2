ca49ddc4b48255b6b1f9dc6c0f80b6c7c94bf974
Patryk <me@parpok.xyz>
2024-07-03 18:39:53 +0200

    Add speed info on vercel and meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	import './style.css';
 
 	import { page } from '$app/stores';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectSpeedInsights();
+	// yes vercel will spy
 
 	const appName = $page.url.hostname;
 	$: title = [appName, ...$page.url.pathname.split('/').slice(1)].filter(Boolean).join(' - ');
@@ -11,6 +15,9 @@
 
 <svelte:head>
 	<title>{title}</title>
+	<meta name="description" content="My personal website" />
+	<meta name="author" content="parpok" />
+	<meta name="keywords" content="{appName}, personal page, links, blog, projects, gallery" />
 	<link rel="icon" href="/avatar.png" />
 	<!-- This is just in case Svelte doesn't catch up with app.html -->
 </svelte:head>
